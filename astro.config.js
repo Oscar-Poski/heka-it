8cb77f3bbbb14046e2be149d04b0b861ba448162
Oscar Rivera <poski@Oscars-MacBook-Air.local>
2026-03-05 17:28:50 -0600

    Fix hooks and switch to Node 24

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
@@ -13,7 +12,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap(),
-    mdx()
+    sitemap()
   ]
 });
